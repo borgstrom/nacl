8ec1ee60bfaadef84388451eb788f8f13cb0c2e3
Evan Borgstrom <evan@borgstrom.ca>
2014-02-14 12:54:30 -0500

    Adding NaCl renderer.
    Greets to @mgwilliams for the initial work!
    https://gist.github.com/mgwilliams/8986670

---
+++ nacl/salt_renderer/nacl.py
@@ -0,0 +1,27 @@
+import logging
+
+try:
+    from nacl.auto import *
+    from nacl.state import default_registry
+    HAS_NACL = True
+except ImportError:
+    HAS_NACL = False
+
+__virtualname__ = 'nacl'
+log = logging.getLogger(__virtualname__)
+
+
+def __virtual__():
+    if HAS_NACL:
+        log.info("NaCl renderer available")
+    return HAS_NACL
+
+
+def render(template, saltenv='base', sls='',
+           tmplpath=None, rendered_sls=None, **kwargs):
+    pillar = __pillar__
+    grains = __grains__
+    salt = __salt__
+
+    exec(template.read())
+    return default_registry.salt_data()
